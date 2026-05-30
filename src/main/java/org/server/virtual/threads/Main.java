b1fbcc869b2dd9f0aeb4b9f70e6c243d8a32d11f
Lomovskoy <lomovskoy.kirill@yandex.ru>
2026-05-30 20:57:14 +0300

    #1 - init

---
+++ src/main/java/org/server/virtual/threads/Main.java
@@ -0,0 +1,7 @@
+package org.server.virtual.threads;
+
+public class Main {
+    static void main() {
+        IO.println("Hello and welcome!");
+    }
+}
---
+++ src/test/java/org/server/virtual/threads/MainTest.java
@@ -0,0 +1,13 @@
+package org.server.virtual.threads;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
